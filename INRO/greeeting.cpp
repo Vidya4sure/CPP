51cb0fa9e52e863e935ecfb812943433af6e8dbf
vidya <hansdavsh@gmail.com>
2024-04-28 16:30:31 +0530

    added a folder

---
+++ INRO/greeeting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	std:: cout << "Enter your name" << std :: endl;
+	std:: string name ;
+	std:: cin >> name ;
+
+	std:: cout << "Enter your age" << std :: endl;
+	int age ;
+	std:: cin >> age ;
+
+	std:: cout << "Hello, " << name << "! You are " << age << std :: endl;
+	return 0 ; 
+}
---
+++ INRO/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+	int num = 1234 ; 
+	int rem = num % 10 ;
+	std::cout << rem ;
+	return 0 ;
+}
